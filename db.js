c86e34f48b94bc0ae31403f20bb954117237435e
Alice088 <alien280579@gmail.com>
2023-11-20 17:36:41 +0300

    add db

---
+++ db.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+});
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import connection from "./db.js"
 const app = express();
 
 app.get('/', (req, res) => {
@@ -10,6 +11,11 @@ app.get("/json", (req, res) => {
 })
 
 const port = process.env.PORT || 3000;
+
+connection.connect(err => {
+    if (err) console.log("no connect", err);
+    else console.log("Successfully connected to the database.");
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
