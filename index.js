7c2e5c8ea351bc292993eab3d3f12edf7fe31274
Alice088 <alien280579@gmail.com>
2023-11-26 17:41:45 +0300

    fix endpoint /authenticationUser

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get("/getUser/:userID", (req, res) => {
 app.get("/authenticationUser/:nickname/:password", (req, res) => {
   connection.query("SELECT password, salt FROM users WHERE nickname = ?", [req.params['nickname']],
     (sqlErr, sqlRes) => {
-      if(sqlErr) res.status(404).json( { message: "User not found", err: sqlErr } )
+      if(sqlErr) res.status(404).json( { message: "User not found", result: false } )
       else {
         const sqlUser = sqlRes[0];
 
